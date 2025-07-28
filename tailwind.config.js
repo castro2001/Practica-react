71832da01d50c475a3072867a7b6bed0f124a2e1
JordanIroute <alexandercastrolino@gmail.com>
2025-07-28 15:58:42 -0500

    Subiendo cambios

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,12 @@
-   module.exports = {
-     darkMode: 'class',
-     // ... otras configuraciones
-   }
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}", // Ajustado para tu estructura
+    "./components/**/*.{js,ts,jsx,tsx}", // si tienes componentes fuera de app
+    "./pages/**/*.{js,ts,jsx,tsx}", // opcional
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
